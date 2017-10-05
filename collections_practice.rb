23c3d510f14ec936f0d595e3f53da09a7c0ccb30
Winter LaMon <winter.lamon@gmail.com>
2017-10-05 14:35:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -86,7 +86,7 @@ def merge_data(keys, data)
       data_hash.collect do |name, attributes|
         if hash_key.values == [name]
           merged_hash << attributes
-          merged_hash.reduce(:merge)
+          merged_hash.reduce!(:merge)
 
        end
 
